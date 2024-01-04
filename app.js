0ef5901e17b40a0d7c3026eaec9c3a22d86ef7a5
Bruno Baêta <brunoantonio25@outlook.com>
2024-01-04 15:28:32 -0300

    add premios e equipe

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const swiper = new Swiper('.swiper', {
 
 //VIDEO SETTINGS - start
 const video = document.querySelector('.banner__video');
-video.play();
+//video.play();
 //VIDEO SETTINGS - end
 
 //CONTADOR - start
